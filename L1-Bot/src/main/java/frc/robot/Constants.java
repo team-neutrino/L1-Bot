70371c385fef46d30e41348680064111047cf224
Neutrino Palkia <neutrino3928@outlook.com>
2025-07-08 19:16:28 -0500

    Intake hopefully should work

--- L1-Bot/src/main/java/frc/robot/Constants.java
+++ L1-Bot/src/main/java/frc/robot/Constants.java
@@ -5,15 +5,32 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+
+  }
+
+  public static class IntakeConstants {
+    public static final int INTAKE_MOTOR_ID = 2;
+    public static final int INTAKE_CURRENT_LIMIT = 40;
+    public static final double INTAKE_kP = 0.0;
+    public static final double INTAKE_kI = 0.0;
+    public static final double INTAKE_kD = 0.0;
+    public static final int COLOR_SENSOR = 1;
+    public static final double INTAKE_VOLTAGE = 1.0;
+    public static final double HOLD_PIECE_VOLTAGE = 0.1;
+    public static final double PROXIMITY = 0.055;
   }
 }
--- L1-Bot/src/main/java/frc/robot/subsystems/Intake.java
+++ L1-Bot/src/main/java/frc/robot/subsystems/Intake.java
@@ -4,19 +4,104 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.reduxrobotics.sensors.canandcolor.Canandcolor;
+import com.reduxrobotics.sensors.canandcolor.CanandcolorSettings;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.IntakeConstants.*;
 
 public class Intake extends SubsystemBase {
+
+    private final CANBus m_CANBus = new CANBus("rio");
+    private TalonFX m_motor = new TalonFX(INTAKE_MOTOR_ID, m_CANBus);
+    private TalonFXConfiguration m_motorConfig = new TalonFXConfiguration();
+    private final CurrentLimitsConfigs m_currentLimitConfig = new CurrentLimitsConfigs();
+    private double m_motorVoltage;
+
+    private Canandcolor m_colorSensor = new Canandcolor(COLOR_SENSOR);
+    private CanandcolorSettings m_settings = new CanandcolorSettings();
+    private Debouncer m_debouncer = new Debouncer(0.1, DebounceType.kFalling);
+    private boolean m_debounced;
+
     public Intake() {
+        m_currentLimitConfig.withSupplyCurrentLimit(INTAKE_CURRENT_LIMIT)
+                .withSupplyCurrentLimitEnable(true)
+                .withStatorCurrentLimit(INTAKE_CURRENT_LIMIT)
+                .withStatorCurrentLimitEnable(true);
+        m_motorConfig.CurrentLimits = m_currentLimitConfig;
+
+        m_motorConfig.Slot0.kP = INTAKE_kP;
+        m_motorConfig.Slot0.kI = INTAKE_kI;
+        m_motorConfig.Slot0.kD = INTAKE_kD;
+
+        m_motor.setNeutralMode(NeutralModeValue.Brake);
+        m_motor.getConfigurator().apply(m_motorConfig);
+
+        m_settings.setLampLEDBrightness(0.01);
+        m_colorSensor.setSettings(m_settings);
+    }
+
+    private boolean withinProximity(double distance) {
+        return m_colorSensor.getProximity() < distance;
+    }
+
+    public boolean hasCoral() {
+        return withinProximity(PROXIMITY);
+    }
+
+    /**
+     * debounced the falling edge of hasCoral for use with scoring commands
+     */
+    public boolean debouncedHasCoral() {
+        return m_debounced;
+    }
+
+    public double getMotorVoltage() {
+        return m_motorVoltage;
     }
 
     @Override
     public void periodic() {
-        // This method will be called once per scheduler run
+        m_motor.set(m_motorVoltage);
+        m_debounced = m_debouncer.calculate(hasCoral());
     }
 
-    @Override
-    public void simulationPeriodic() {
-        // This method will be called once per scheduler run during simulation
+    public void setVoltage(double volts) {
+        m_motorVoltage = volts;
     }
+
+    /**
+     * Gives an instance of the coral default command. Stops intake from running
+     * 
+     * @return The coral default command
+     */
+    public Command intakeDefaultCommand() {
+        return run(() -> {
+            if (debouncedHasCoral()) {
+                m_motorVoltage = HOLD_PIECE_VOLTAGE;
+            } else {
+                m_motorVoltage = 0;
+            }
+        });
+    }
+
+    /**
+     * Gives an instance of the run intake command., sets the intake voltage to the
+     * speed provided.
+     * 
+     * @param speed speed coral is set to
+     * @return The run intake command
+     */
+    public Command runIntake(double speed) {
+        return run(() -> m_motorVoltage = speed);
+    }
+
 }
