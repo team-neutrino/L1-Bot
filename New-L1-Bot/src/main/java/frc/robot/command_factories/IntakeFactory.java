c93361f19ee8083235001944a2b4740b7aec8bcf
Neutrino Palkia <neutrino3928@outlook.com>
2025-08-14 17:10:37 -0500

    copy new l1

---
+++ New-L1-Bot/src/main/java/frc/robot/command_factories/IntakeFactory.java
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.command_factories;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+import static frc.robot.util.Subsystem.intake;
+
+import static frc.robot.Constants.IntakeConstants.*;
+
+/** Add your docs here. */
+public class IntakeFactory {
+    public static Command runIntake() {
+        return intake.runIntake(INTAKE_VOLTAGE).until(() -> intake.hasCoral());
+    }
+
+    public static Command runOuttake() {
+        return intake.runIntake(OUTTAKE_VOLTAGE);
+    }
+}
