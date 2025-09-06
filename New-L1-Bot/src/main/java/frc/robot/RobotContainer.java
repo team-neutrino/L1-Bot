38644ea09d456ab58d7fffd117def404d5a88afa
Dialga-Neutrino <neutrino3928@outlook.com>
2025-09-06 10:42:20 -0500

    issues

--- New-L1-Bot/src/main/java/frc/robot/RobotContainer.java
+++ New-L1-Bot/src/main/java/frc/robot/RobotContainer.java
@@ -53,32 +53,31 @@ public class RobotContainer {
                 m_buttonController.a().whileTrue(IntakeFactory.runIntake());
                 m_buttonController.b().whileTrue(ArmFactory.IntakePosition());
 
+                m_driverController.a().whileTrue(drivetrain.applyRequest(() -> brake));
+                m_driverController.b().whileTrue(drivetrain.applyRequest(() -> point.withModuleDirection(
+                                new Rotation2d(-m_driverController.getLeftY(), -m_driverController.getLeftX()))));
+
+                // Run SysId routines when holding back/start and X/Y.
+                // Note that each routine should be run exactly once in a single log.
+                m_driverController.back().and(m_driverController.y())
+                                .whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+                m_driverController.back().and(m_driverController.x())
+                                .whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+                m_driverController.start().and(m_driverController.y())
+                                .whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+                m_driverController.start().and(m_driverController.x())
+                                .whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+
+                // reset the field-centric heading on left bumper press
+                m_driverController.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+
+                drivetrain.registerTelemetry(logger::telemeterize);
+
+                m_buttonController.x().whileTrue(SuperstructureFactory.IntakeCoral());
+                m_buttonController.y().whileTrue(SuperstructureFactory.ScoreCoral());
         }
 
-        m_driverController.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        m_driverController.b().whileTrue(drivetrain.applyRequest(
-                () -> point.withModuleDirection(
-                        new Rotation2d(-m_driverController.getLeftY(), -m_driverController.getLeftX()))));
-
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
-        m_driverController.back().and(m_driverController.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        m_driverController.back().and(m_driverController.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        m_driverController.start().and(m_driverController.y())
-                .whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        m_driverController.start().and(m_driverController.x())
-                .whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
-
-        // reset the field-centric heading on left bumper press
-        m_driverController.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
-
-        drivetrain.registerTelemetry(logger::telemeterize);
-
-        m_buttonController.x().whileTrue(SuperstructureFactory.IntakeCoral());
-        m_buttonController.y().whileTrue(SuperstructureFactory.ScoreCoral());
-    }
-
-    public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
-    }
+        public Command getAutonomousCommand() {
+                return Commands.print("No autonomous command configured");
+        }
 }
