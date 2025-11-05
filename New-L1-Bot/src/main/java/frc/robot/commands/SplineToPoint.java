1de5a9c98bc8230c9570715b008c3f70d52926b3
Neutrino Angus <neutrino3928@outlook.com>
2025-11-04 19:44:22 -0600

    on the fly pathing works in sim

--- New-L1-Bot/src/main/java/frc/robot/commands/SplineToPoint.java
+++ New-L1-Bot/src/main/java/frc/robot/commands/SplineToPoint.java
@@ -23,39 +23,29 @@ import frc.robot.util.Subsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class SplineToPoint extends Command {
-  private List<Waypoint> m_waypoints;
   private Command m_pathCommand;
-  private RobotConfig m_config;
+  private Pose2d m_target;
+  PathConstraints m_constraints;
 
   /** Creates a new Splinetopoint. */
   public SplineToPoint() {
     // Use addRequirements() here to declare subsystem dependencies.
-    m_config = Subsystem.swerve.getRobotConfig();
+    addRequirements(Subsystem.swerve);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_waypoints = PathPlannerPath.waypointsFromPoses(
-        Subsystem.swerve.getCurrentPose(),
-        new Pose2d(9.0, 4.0, Rotation2d.fromDegrees(0)));
+    m_constraints = new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI);
+    m_target = new Pose2d(6, 7, new Rotation2d(0));
 
-    PathConstraints m_constraints = new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI);
-
-    PathPlannerPath path = new PathPlannerPath(
-        m_waypoints,
-        m_constraints,
-        null,
-        new GoalEndState(0.0, Rotation2d.fromDegrees(-90)));
-
-    path.generateTrajectory(new ChassisSpeeds(), new Rotation2d(), m_config);
-    m_pathCommand = AutoBuilder.followPath(path);
+    m_pathCommand = AutoBuilder.pathfindToPose(m_target, m_constraints);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_pathCommand.execute();
+    m_pathCommand.schedule();
   }
 
   // Called once the command ends or is interrupted.
